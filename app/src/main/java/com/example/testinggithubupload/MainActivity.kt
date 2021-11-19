2410048bf209ea60c200efee754551b54cb6ff64
Studiop1 <sriehle4@ivytech.edu>
2021-11-19 18:24:12 -0500

    Try2

--- app/src/main/java/com/example/testinggithubupload/MainActivity.kt
+++ app/src/main/java/com/example/testinggithubupload/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.testinggithubupload
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//edited code to add a comment
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}
+}//Newer comment
