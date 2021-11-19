25967ec03dabcbf790803e37954080f57f21c344
Studiop1 <85195210+Studiop1@users.noreply.github.com>
2021-11-19 18:13:45 -0500

    Update MainActivity.kt
    Just testing by adding a comment

--- app/src/main/java/com/example/testinggithubupload/MainActivity.kt
+++ app/src/main/java/com/example/testinggithubupload/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.testinggithubupload
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//edited code to add a comment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
