268187a347317729dd9f01d8c2b753ac049fa43d
Studiop1 <sriehle4@ivytech.edu>
2021-11-19 18:25:47 -0500

    Try3

--- app/src/main/java/com/example/testinggithubupload/MainActivity.kt
+++ app/src/main/java/com/example/testinggithubupload/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.testinggithubupload
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Comment added in AS
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
